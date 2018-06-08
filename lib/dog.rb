decf2c4b1511d6b8b42a9061c4c32575005d36d3
amemonkey <ameliafunaki789@gmail.com>
2018-06-08 20:29:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,11 @@
 # dog.rb
+class Dog
+  def initialize(name)
+    @name
+  end
+
+end
+
+fido = Dog.new("fido")
+lassie = Dog.new("lassie")
+snoopy = Dog.new("snoopy")
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,10 @@
 # person.rb
+# dog.rb
+class Person 
+  def initialize(first_last)
+    @first_last
+  end
+end
+
+adele_goldberg = Person.new("adele_goldberg")
+alan_kay = Person.new("alan_kay")
